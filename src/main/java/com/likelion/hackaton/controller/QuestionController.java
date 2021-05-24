34cccc50e2623b50c416bd10b930926e58617b1e
DESKTOP-NRTTKAM\daehwan <daehwan7989@naver.com>
2021-05-24 10:26:54 +0900

    #1 testPage 데이터 삽입 완성

---
+++ src/main/java/com/likelion/hackaton/controller/QuestionController.java
@@ -0,0 +1,29 @@
+package com.likelion.hackaton.controller;
+
+import com.likelion.hackaton.form.QuestionForm;
+import com.likelion.hackaton.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/question/{questionNo}")
+    public String nextQuestion(@PathVariable("questionNo")int questionNo,
+                               Model model){
+        QuestionForm questionForm = questionService.getQuestion(questionNo);
+        model.addAttribute("questionForm",questionForm);
+        log.info("get quesion : "+questionNo);
+        return "question";
+    }
+}
